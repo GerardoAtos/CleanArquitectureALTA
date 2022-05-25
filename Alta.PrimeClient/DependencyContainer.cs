24daf2a26f6e2a0a168de9765a21e84224ed29c1
alexdelgadillos <alejandro.delgadillo@atos.net>
2022-05-25 11:38:36 -0500

    Added Polly on DependencyContainer

--- Alta.PrimeClient/DependencyContainer.cs
+++ Alta.PrimeClient/DependencyContainer.cs
@@ -2,19 +2,50 @@
 using Alta.Entities.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Timeout;
+using System;
+using System.Net.Http;
 
 namespace Alta.PrimeClient
 {
     public static class DependencyContainer
     {
-        public static IServiceCollection  AddPrimeClientServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddPrimeClientServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<ILoggingRepository, ConsoleLoggingRepository>();
             services.AddScoped<IPrimeClient, HttpPrimeClient>();
-            services.AddHttpClient<IPrimeClient, HttpPrimeClient>();
-
+            AddClientWithPolicies(services);
             services.Configure<PrimeWsOptions>(configuration.GetSection(PrimeWsOptions.PrimeWs));
             return services;
+        }
+
+        private static void AddClientWithPolicies(IServiceCollection services)
+        {
+            Random jitterer = new Random();
+            services.AddHttpClient<IPrimeClient, HttpPrimeClient>().AddTransientHttpErrorPolicy(builder =>
+          builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+              5,
+              retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0,1000)),
+              onRetry: (outcome, timeSpan, retryAttempt) =>
+              {
+                  //onretry to do
+                  Console.WriteLine("Retry");
+              }
+            )).AddTransientHttpErrorPolicy(builder =>
+          builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(3,
+          TimeSpan.FromSeconds(15),
+          onBreak: (outcome, timeSpan) =>
+          {
+              //onbreak to do 
+              Console.WriteLine("Break");
+          },
+          onReset: () =>
+          {
+              //onreset to do 
+              Console.WriteLine("Reset");
+          }
+          )).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
         }
     }
 }
